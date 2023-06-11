9eef54d4cba53df9986eedbede7f6182714814f4
unknown <chrissas02@gmail.com>
2023-06-11 16:58:40 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,43 @@
+import java.io.File;
 public class Driver { 
  public static void main(String [] args) { 
   Polynomial p = new Polynomial(); 
   System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
+  double [] c1 = {6,5};
+  int [] e1 = {0,3}; 
+  Polynomial p1 = new Polynomial(c1,e1); 
+  double [] c2 = {-2,-9};
+  int [] e2 = {1,4}; 
+  Polynomial p2 = new Polynomial(c2,e2); 
   Polynomial s = p1.add(p2); 
   System.out.println("s(0.1) = " + s.evaluate(0.1)); 
   if(s.hasRoot(1)) 
    System.out.println("1 is a root of s"); 
   else 
    System.out.println("1 is not a root of s"); 
+  double [] c3 = {3,4};
+  int [] e3 = {1,2};
+  Polynomial p3 = new Polynomial(c3,e3);
+  double [] c4 = {4};
+  int [] e4 = {1};
+  Polynomial p4 = new Polynomial(c4,e4);
+  Polynomial s2 = p3.multiply(p4); 
+  System.out.println("s(1) = " + s2.evaluate(1));
+  File file = new File("text.txt");
+  try{
+	Polynomial filePoly = new Polynomial(file);
+        System.out.println("Coefficients:");
+	for(double coeff: filePoly.coef){
+		System.out.println(coeff);
+	}
+	System.out.println("Exponents:");
+	for(double expo: filePoly.exp){
+		System.out.println(expo);
+	}
+	filePoly.saveToFile("save.txt");
+  }
+  catch(Exception e){
+	System.out.println(e);
+  }
  } 
 } 
--- Polynomial.java
+++ Polynomial.java
@@ -1,13 +1,62 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Polynomial{
 	double [] coef;
+	int [] exp;
 	public Polynomial(){
-		coef = new double[1];
-		coef[0] = 0;
+		this.coef = null;
+		this.exp = null;
 	}
-	public Polynomial(double[] arr){
-		coef = new double[arr.length];
-		for(int i=0;i<arr.length;i++){
-			coef[i] = arr[i];
+	public Polynomial(double[] coef, int[] exp){
+		this.coef = coef;
+		this.exp = exp;
+	}
+	public Polynomial(File file) throws Exception{
+		Scanner myScanner = new Scanner(file);
+		if(!myScanner.hasNextLine()){
+			this.coef = null;
+			this.exp = null;
+		}
+		else{
+			String line = myScanner.nextLine();
+			line = line.replace("-", "+-");	
+			String[] poly_arr = line.split("\\+");
+			this.exp = new int[poly_arr.length];
+			this.coef = new double[poly_arr.length];
+			for(int i = 0; i < poly_arr.length; i++){
+				String[] subArray = poly_arr[i].split("x");
+				coef[i] = Double.parseDouble(subArray[0]);
+				if(subArray.length >1){
+					exp[i] = Integer.parseInt(subArray[1]);
+				}
+				else{
+					exp[i] = 0;
+				}
+			} 
+		}
+		myScanner.close();
+	}
+	public void saveToFile(String myFile) throws Exception{
+		if(this.coef == null) return;
+		if(this.coef.length != this.exp.length){
+		}
+		else{
+			String writeString = "";
+			for(int i = 0; i < this.coef.length; i++){
+				writeString += coef[i];
+				if(exp[i] != 0){
+					writeString += "x" +exp[i];
+				}
+				writeString += "+";
+			}
+			if(writeString.endsWith("+")){
+				writeString = writeString.substring(0, writeString.length()-1);
+			}
+			FileWriter myWriter = new FileWriter(new File(myFile));
+			myWriter.write(writeString);
+			myWriter.close();
 		}
 	}
 	public double getC(int i){
@@ -17,37 +66,129 @@ public class Polynomial{
 		return coef.length;
 	}
 	public Polynomial add(Polynomial s){
-		int a = getD();
-		int b = s.getD();
-		if(a>b){
-			Polynomial p = new Polynomial(coef);
-			for(int i = 0; i<b;i++){
-				p.coef[i] = p.coef[i] + s.coef[i];
+		int highest = 0;
+		for(int i = 0; i<exp.length; i++){
+			if(exp[i]>highest){
+				highest = exp[i];
 			}
-			return p;
 		}
-		else{
-			double[] arr = new double[b];
-			for(int i=0;i<b;i++){
-				arr[i]=s.getC(i);
+		for(int i = 0; i<s.exp.length; i++){
+			if(s.exp[i]>highest){
+				highest = s.exp[i];
 			}
-			Polynomial p = new Polynomial(arr);
-			for(int i = 0; i<a;i++){
-				p.coef[i] = p.coef[i] + coef[i];
-			}
-			return p;
 		}
+		double[] arr = new double[highest+1];
+		int[] arr2 = new int[highest+1];
+		for(int i = 0; i<highest+1; i++){
+			for(int j = 0; j<exp.length; j++){
+				if(i == exp[j]){
+					if(arr2[i] == exp[j]){
+						arr[i]=arr[i]+coef[j];	
+					}
+					else{
+						arr[i]=coef[j];
+						arr2[i]=exp[j];
+					}
+				}	
+			}
+			for(int j = 0; j<s.exp.length; j++){
+				if(i == s.exp[j]){
+					if(arr2[i] == s.exp[j]){
+						arr[i]=arr[i]+s.coef[j];	
+					}
+					else{
+						arr[i]=s.coef[j];
+						arr2[i]=s.exp[j];
+					}
+				}	
+			}
+		}
+		int x=0;
+		for(int i = 0; i<highest+1; i++){
+			if((arr[i] == 0.0) && (arr2[i] == 0)){
+			}
+			else{
+				x=x+1;
+			}
+		}
+		double[] fin = new double[x];
+		int[] fin2 = new int[x];
+		int y=0;
+		for(int i = 0; i<highest+1; i++){
+			if((arr[i] == 0.0) && (arr2[i] == 0)){
+			}
+			else{
+				fin[y]=arr[i];
+				fin2[y]=arr2[i];
+				y=y+1;
+			}
+		}
+		Polynomial p = new Polynomial(fin, fin2);
+		return p;
+	}
+	public Polynomial multiply(Polynomial s){
+		int highest = 0;
+		for(int i = 0; i<exp.length; i++){
+			if(exp[i]>highest){
+				highest = exp[i];
+			}
+		}
+		for(int i = 0; i<s.exp.length; i++){
+			if(s.exp[i]>highest){
+				highest = s.exp[i];
+			}
+		}
+		double[] arr = new double[highest+1];
+		int[] arr2 = new int[highest+1];
+		for(int i = 0; i<highest+1; i++){
+			for(int j = 0; j<exp.length; j++){
+				if(i == exp[j]){
+					arr[i]=coef[j];
+					arr2[i]=exp[j];
+				}	
+			}
+		}
+		for(int i = 0; i<highest+1; i++){
+			for(int j = 0; j<s.exp.length; j++){
+				if((arr[i] == 0.0) && (arr2[i] == 0)){
+				}
+				else{
+					arr[i]=arr[i]*s.coef[j];
+					arr2[i]=arr2[i]+s.exp[j];
+				}
+			}
+		}
+		int x=0;
+		for(int i = 0; i<highest+1; i++){
+			if((arr[i] == 0.0) && (arr2[i] == 0)){
+			}
+			else{
+				x=x+1;
+			}
+		}
+		double[] fin = new double[x];
+		int[] fin2 = new int[x];
+		int y=0;
+		for(int i = 0; i<highest+1; i++){
+			if((arr[i] == 0.0) && (arr2[i] == 0)){
+			}
+			else{
+				fin[y]=arr[i];
+				fin2[y]=arr2[i];
+				y=y+1;
+			}
+		}
+		Polynomial p = new Polynomial(fin, fin2);
+		return p;
 	}
 	public double evaluate(double x){
+		if(coef == null) return 0;
+		if(coef.length != exp.length){
+			return 0;
+		}
 		double result = 0;
-		int degree = 0;
 		for(int i=0;i<coef.length;i++){
-			double n=1;
-			for(int j =0;j<degree;j++){
-				n=n*x;
-			}
-			result = result + coef[i]*n;
-			degree=degree+1;
+			result = result + coef[i]*Math.pow(x, exp[i]);
 		}
 		return result;
 	}
